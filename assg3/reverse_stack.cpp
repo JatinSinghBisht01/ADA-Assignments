c79a4601d66b6f21c8f9a26f4b09d958fc14c72d
AVI GARG_21105032 <97087858+AvixGarg@users.noreply.github.com>
2023-04-28 10:19:23 +0530

    Update reverse_stack.cpp

--- assg3/reverse_stack.cpp
+++ assg3/reverse_stack.cpp
@@ -1,9 +1,9 @@
-// Program 1: Given a Stack 'S'. Write a Python Program to SORT the stack recursively in decreasing order.
+// Program 1: Given a Stack 'S'. Write a Python Program to reverse the stack recursively.
 // Note: Looping through the stack is not allowed. You need to return a stack that is sorted in decreasing order.
 
 // Example:
 // Given stack: 5 1 2 6 4
-// Output: 6 5 4 2 1
+// Output: 4 6 2 1 5
 
 #include<bits/stdc++.h>
 using namespace std;
